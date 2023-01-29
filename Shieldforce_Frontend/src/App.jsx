78af72ae1e79e9cdfc14603d00a4e34ab4e3c197
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2023-01-30 02:09:33 +0400

    Home page

--- Shieldforce_Frontend/src/App.jsx
+++ Shieldforce_Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavbarComp from "./components/NavbarComp";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
