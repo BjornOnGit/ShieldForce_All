8c6ce00f5a689c575b98888662f9efcebc198d02
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2023-01-29 22:25:39 +0400

    Added the remaining routes

--- Shieldforce_Frontend/src/App.jsx
+++ Shieldforce_Frontend/src/App.jsx
@@ -6,12 +6,18 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Signup from "./pages/Signup";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
 }
