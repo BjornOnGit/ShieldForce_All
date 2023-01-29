d09738b9238864d7b85c39a945cb9ecae9ccdd45
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2023-01-29 22:13:47 +0400

    Made the basic layout, and set up routing

--- Shieldforce_Frontend/src/App.jsx
+++ Shieldforce_Frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComp from './components/NavbarComp';
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComp from "./components/NavbarComp";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <NavbarComp/>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
